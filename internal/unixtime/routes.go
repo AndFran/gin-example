f311ba24afa11e950420c9c4108c256dced9a1b4
AndFran <andrewwilliamfranks@gmail.com>
2024-01-22 16:19:36 +0100

    Update routes.go

--- internal/unixtime/routes.go
+++ internal/unixtime/routes.go
@@ -1,1 +1,12 @@
+package unixtime
+
+import "github.com/gin-gonic/gin"
+
+// curl localhost:3000/unixtime/3523463425
+
+func RegisterRoutes(r *gin.Engine) {
+	var h handler
+	routesUnixtime := r.Group("/unixtime")
+	routesUnixtime.GET("/:timestamp", h.toHuman)
+}
 
