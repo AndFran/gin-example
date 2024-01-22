f7e002ef2def1fe523ed9c34ff974512d92377f3
AndFran <andrewwilliamfranks@gmail.com>
2024-01-22 16:19:09 +0100

    Update logger.go

--- internal/httpmiddleware/logger.go
+++ internal/httpmiddleware/logger.go
@@ -1,1 +1,16 @@
+package httpmiddleware
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
+
+func LogRequest() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		guid, _ := c.Get("uuid")
+		fmt.Printf("The request with uuid %s started\n", guid.(string))
+		c.Next() // next in chain
+		fmt.Printf("The request with uuid %s finished\n", guid.(string))
+	}
+}
 
