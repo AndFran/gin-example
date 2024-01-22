d145be5f2e335b01a64a26a2170215322737d154
AndFran <andrewwilliamfranks@gmail.com>
2024-01-22 16:18:58 +0100

    Update headers.go

--- internal/httpmiddleware/headers.go
+++ internal/httpmiddleware/headers.go
@@ -1,1 +1,15 @@
+package httpmiddleware
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
+
+func SetRequestIdHeader() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		guid := uuid.New().String()
+		c.Set("uuid", guid)
+		c.Header("X-Request-id", guid)
+	}
+}
 
