fcce41497dadad6654bd5f03c05620484af52292
AndFran <andrewwilliamfranks@gmail.com>
2024-01-22 16:18:46 +0100

    Update routes.go

--- internal/b64/routes.go
+++ internal/b64/routes.go
@@ -1,1 +1,14 @@
+package b64
+
+import "github.com/gin-gonic/gin"
+
+// curl "127.0.0.1:3000/base64/encode?text=hello"
+// curl "127.0.0.1:3000/base64/decode?base64=aGVsbG8="
+
+func RegisterRoutes(r *gin.Engine) {
+	h := &handler{}
+	routesBase64 := r.Group("/base64")
+	routesBase64.GET("/decode", h.decode)
+	routesBase64.GET("/encode", h.encode)
+}
 
